5146d9c6b9715a39b2f2eb146c8165ffcbd0cc25
erdemcagri <48623849+erdemcagri@users.noreply.github.com>
2020-01-10 15:47:21 +0100

    Add files via upload

--- test_Password.py
+++ test_Password.py
@@ -1,7 +1,7 @@
 import unittest
 from Password import Password as p
 
-class test_password(unittest.TestCase):
+class testPassword(unittest.TestCase):
     def test_check_complexity(self):
         self.assertTrue(p.check_complexity(True, "1Pp4ssw0rdD"))
         self.assertNotEqual(p.check_complexity(False, "password"), True)    
