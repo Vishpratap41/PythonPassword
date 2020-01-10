aaae292afaf01da125da90f04c5fb38aa5229658
erdemcagri <48623849+erdemcagri@users.noreply.github.com>
2020-01-10 14:15:36 +0100

    Add files via upload

---
+++ test_Password.py
@@ -0,0 +1,14 @@
+import unittest
+from Password import Password as p
+
+class testPassword(unittest.TestCase):
+    def test_check_complexity(self):
+        self.assertTrue(p.check_complexity(True, '1Pp4ssw0rdD'))
+        self.assertNotEqual(p.check_complexity(False, 'password'), True)
+    #def test_hash_password(self):
+    #    True
+    
+    
+
+if __name__ == '__main__':
+    unittest.main()
